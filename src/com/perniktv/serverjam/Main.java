21ccc2ebe21029ae38b3598204fa6de83405c500
Tapuzimo <46405044+Tapuzimo@users.noreply.github.com>
2020-07-03 11:56:22 +0300

    Update Main.java

--- src/com/perniktv/serverjam/Main.java
+++ src/com/perniktv/serverjam/Main.java
@@ -1,19 +1,42 @@
 package com.perniktv.serverjam;
 
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.perniktv.serverjam.events.NoAchievements;
 
 public class Main extends JavaPlugin {
 	
+	public static Main plugin;
+	private Logger log = Logger.getLogger("minecraft");
+	
 	@Override
 	public void onEnable() {
-		// TODO Auto-generated method stub
+		plugin = this;
+		registerCommands();
+		registerEvents();
 		super.onEnable();
 	}
 	
 	@Override
 	public void onDisable() {
-		// TODO Auto-generated method stub
+		plugin = null;
 		super.onDisable();
+	}
+	
+	private void registerCommands() {
+		
+	}
+	
+	private void registerEvents() {
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(new NoAchievements(), this);
+	}
+	
+	public static Main getPlugin() {
+		return plugin;
 	}
 
 }
